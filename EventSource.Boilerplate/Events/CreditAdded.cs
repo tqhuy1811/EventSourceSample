909e48fce6c91d0df856a5f179e0e87acbe13f5a
Huy <tqhuy1811@gmail.com>
2020-03-08 23:42:23 +0700

    update domain event

--- EventSource.Boilerplate/Events/CreditAdded.cs
+++ EventSource.Boilerplate/Events/CreditAdded.cs
@@ -1,6 +1,19 @@
-﻿namespace EventSource.Boilerplate.Events
+﻿using System;
+
+namespace EventSource.Boilerplate.Events
 {
 	public class CreditAdded: IDomainEvent
 	{
+		public CreditAdded(
+			Guid aggregateId,
+			Money credit)
+		{
+			Id = aggregateId;
+			Credit = credit;
+		}
+
+		public Guid Id { get; }
+		public Money Credit { get; }
+		
 	}
 }
--- EventSource.Boilerplate/Events/PhoneCallCharged.cs
+++ EventSource.Boilerplate/Events/PhoneCallCharged.cs
@@ -1,6 +1,27 @@
-﻿namespace EventSource.Boilerplate.Events
+﻿using System;
+
+namespace EventSource.Boilerplate.Events
 {
-	public class PhoneCallCharged
+	public class PhoneCallCharged: IDomainEvent
 	{
+		public PhoneCallCharged(
+			Guid aggregateId,
+			PhoneCall phoneCall,
+			Money costOfCall,
+			Minutes coveredByAllowance)
+		{
+			Id = aggregateId;
+			PhoneCall = phoneCall;
+			CostOfCall = costOfCall;
+			CoveredByAllowance = coveredByAllowance;
+		}
+
+		public Guid Id { get;  }
+		
+		public PhoneCall PhoneCall { get; }
+
+		public Money CostOfCall { get; }
+
+		public Minutes CoveredByAllowance { get; }
 	}
 }
